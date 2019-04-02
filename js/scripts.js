95290fb69a53b0f76e71ef41c0e1b95c7b89363c
Heather Zurek and megan schulte <megan@>
2019-04-02 15:19:03 -0700

    add third response to index.html and second checked function to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
-  $("$stressForm").submit(function(event) {
+  $("#stressForm").submit(function(event) {
     event.preventDefault();
 
     $("input:checkbox[name=warningSigns]:checked").each(function() {
-      var warningSigns = $(this).val();
+    var warningSigns = $(this).val();
+    $("input:checkbox[name=symptoms]:checked").each(function() {
+      var symptoms = $(this).val();
 
     if (warningSigns === "window" && warningSigns === "sleep" && symptoms === "sick") {
       $("#testResponses1").show();
-      $("#testResponses2, testResponses3").hide();
-    } else if ()
+      $("#testResponses2, #testResponses3").hide();
+    } else if (warningSigns === "emotion" || warningSigns === "anger" || symptoms === "brain") {
+      $("#testResponses2").show();
+      $("#testResponses1, #testResponses3").hide();
+    } else {
+      $("#testResponses3").show();
+      $("#testResponses1, #testResponses2").hide();
+    }
 
   });
-
+    });
+  });
 });
